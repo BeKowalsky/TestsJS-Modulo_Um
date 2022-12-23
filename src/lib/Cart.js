dbd1227d5298332153d675bb61573710c3520793
Bernardo Magueta Kowalsky <kwybernardo@gmail.com>
2022-12-23 13:57:34 -0300

    Implementação do lodash

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,7 +1,17 @@
+import find from 'lodash/find';
+import remove from 'lodash/remove';
+
 export default class Cart {
   items = [];
 
   add(item) {
+    const itemToFind = { product: item.product };
+
+    if (find(this.items, itemToFind)) {
+      //Retorna o primeiro item que corresponde ao valor, nesse caso primeira chamada da classe vai criar o item, então não cai no find, já a segunda vai ser existente, então cai nesse if;
+      remove(this.items, itemToFind); // Remove o item que retornou no find;
+    }
+
     this.items.push(item);
   }
 
