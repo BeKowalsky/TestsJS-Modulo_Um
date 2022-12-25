600c381e17de4f291d6fae590aa38e63b6dae07c
Bernardo Magueta Kowalsky <kwybernardo@gmail.com>
2022-12-25 17:01:15 -0300

    Uso da biblioteca Dinero.js

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,5 +1,11 @@
 import find from 'lodash/find';
 import remove from 'lodash/remove';
+import Dinero from 'dinero.js';
+
+const Money = Dinero; //Atribui todos os valores de 'Dinero' para 'Money', serve para na hora de usar, ao invés de colocar 'Dinero', coloca 'Money'
+
+Money.defaultCurrency = 'BRL'; // Padrão em reais
+Money.defaultPrecision = 2; // Até 2 casas após vírgula
 
 export default class Cart {
   items = [];
@@ -20,12 +26,15 @@ export default class Cart {
 
   getTotal() {
     return this.items.reduce((acumulator, item) => {
-      return acumulator + item.quantity * item.product.price;
-    }, 0);
+      return acumulator.add(
+        //Acumulator agora usa o add para fazer o cálculo ao passar pelos itens do array
+        Money({ amount: item.quantity * item.product.price }),
+      );
+    }, Money({ amount: 0 })); // Mudou de 0 para R$0.0
   }
 
   summary() {
-    const total = this.getTotal();
+    const total = this.getTotal().getAmount();
     const items = this.items;
 
     return {
